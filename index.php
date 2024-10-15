7f8aa29caa0fa6d2d9a621c780142116c7af405a
ibrahim78-cyber <ibrahimshar1516@gmail.com>
2024-10-15 22:52:09 +0500

    addes index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+echo "<html>
+    <head>
+
+    </head>
+    <body>
+    <h1>Stars Online Academy</h1>
+    <p>In this online academy,online courses will be available .Clients can buy any online course,upload,blogs.Moreover there would be about section in which info related to website would be available.There would be contact us option so that client can contact us in case of any query.</p>    
+    </body>
+</html>";
+
+$members=array("Muhammad Mughees AhmAd",
+"Muhammad Ibrahim",
+"Kinza Irshad");
+echo "Following are the group members for this project:" . "<br>";
+echo $members[0];
+echo "<br>";
+echo $members[1];
+echo "<br>";
+echo $members[2];
+echo "<br>";
+?>
